db0e388806392155fa8f101f727c7945eab03678
Всеволод Донченко <vsevolond@MacBook-Air-Vsevolod.local>
2025-02-20 13:48:18 +0300

    PROD: started project

---
+++ PDFImageCreator/PDFImageCreator/PDFImageCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFImageCreatorApp.swift
+//  PDFImageCreator
+//
+//  Created by Всеволод Донченко on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFImageCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
